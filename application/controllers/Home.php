68b49df044093373e2a17974872d2f170730e811
mochandri <72057874+mochandri@users.noreply.github.com>
2023-09-27 21:42:12 +0700

    up

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,6 +5,10 @@ class Home extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('templates/index');
+		$this->load->view('templates/header');
+        $this->load->view('templates/sidebar');
+        $this->load->view('templates/topbar');
+        $this->load->view('templates/index');
+        $this->load->view('templates/footer');
 	}
 }
